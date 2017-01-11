7b8a878c7e560f4faa65e28d09a28ee41287c692
Tyler Rockwell <tyler.rockwell87@gmail.com>
2017-01-11 14:59:33 -0500

    Update code in vision.py to meet pep8 standards

--- vision.py
+++ vision.py
@@ -1,31 +1,32 @@
-from numpy import *
+from numpy import array
 from PIL import ImageOps
-from PIL import Image # Remove once all recipes have been coded
+from PIL import Image  # Remove once all recipes have been coded
 import pyscreenshot
 
-class Vision:
-    def __init__(self, gameLocation):
-        self.gameLocation = gameLocation
 
-    def screenGrab(self):
-        box = self.gameWindow()
+class Vision:
+    def __init__(self, game_location):
+        self.game_location = game_location
+
+    def analyze(self, box):
+        image = self._screen_grab()
+        # image = Image.open('screenshot.png') # Remove once all recipes have been coded
+        order_image = image.crop(box)
+        order_image = ImageOps.grayscale(order_image)
+        value = array(order_image.getcolors()).sum()
+        # print value
+        return value
+
+    def _screen_grab(self):
+        box = self.game_window()
         image = pyscreenshot.grab(box, backend='mac_screencapture')
         return image
 
-    def gameWindow(self):
+    def _game_window(self):
         window = (
-                   self.gameLocation.xOffset,
-                   self.gameLocation.yOffset,
-                   self.gameLocation.offsetWidth(),
-                   self.gameLocation.offsetHeight()
+                   self.game_location.xOffset,
+                   self.game_location.yOffset,
+                   self.game_location.offsetWidth(),
+                   self.game_location.offsetHeight()
               )
         return window
-
-    def analyze(self, box):
-        image = self.screenGrab()
-        # image = Image.open('screenshot.png') # Remove once all recipes have been coded
-        orderImage = image.crop(box)
-        orderImage = ImageOps.grayscale(orderImage)
-        value = array(orderImage.getcolors()).sum()
-        # print value
-        return value
